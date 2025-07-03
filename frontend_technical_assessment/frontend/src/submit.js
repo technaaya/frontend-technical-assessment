bd80fb16cf9b3ce238c7b5e80f170a1eb7c74d55
technaaya <129490194+technaaya@users.noreply.github.com>
2025-07-03 15:43:07 +0530

    Delete frontend_technical_assessment directory

--- frontend_technical_assessment/frontend/src/submit.js
+++
@@ -1,8 +0,0 @@
-// submit.js
-
-export const SubmitButton = ({submitData}) => {
-
-    return (
-        <button onClick={()=>submitData()} className="fixed bottom-16 left-[50%] translate-x-[-50%] min-w-[100px] text-center font-semibold text-white bg-[#1C2536] px-4 py-2 rounded-[8px] cursor-grab" type="submit">Submit</button>
-    );
-}
